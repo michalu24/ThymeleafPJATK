a57fa72c39ca811fcdf6153880b4a8519e903d87
michalu24 <it.michal.urbanski@gmail.com>
2025-03-23 15:12:03 +0100

    Refactor main routing and add Thymeleaf fragments.
    Updated `MainController` to accept path parameters and pass them to the view. Introduced multiple Thymeleaf fragments for modular and reusable HTML structures. Enhanced `index.html` to dynamically load these fragments based on the provided route.

--- src/main/java/org/example/thymeleafpjatk/controllers/MainController.java
+++ src/main/java/org/example/thymeleafpjatk/controllers/MainController.java
@@ -5,6 +5,7 @@ import org.example.thymeleafpjatk.model.index.Properties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @AllArgsConstructor
@@ -12,9 +13,10 @@ public class MainController {
 
     private final Properties properties;
 
-    @GetMapping("/")
-    public String mainPage(Model model) {
+    @GetMapping("/{name}")
+    public String mainPage(@PathVariable("name") String fragmentType, Model model) {
         model.addAttribute("props", properties);
+        model.addAttribute("fragmentType", fragmentType);
         return "index";
     }
 }
